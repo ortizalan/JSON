7e22ba5f737230c750c0e851a3703f808808dda7
Alan Ortíz Perez <aortiz@isssteson.gob.mx>
2016-09-27 14:52:32 -0700

    Inicio Projecto

---
+++ WindowsFormsApplication1/Clases/Fallecido.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication1.Clases
+{
+    class Fallecido
+    {
+        public string Curp { get; set; }
+
+        public string Nombre { get; set; }
+
+        public string Paterno { get; set; }
+
+        public string Materno { get; set; }
+
+        public int Edad { get; set; }
+
+        public Fallecido(string curp, string nombre, string paterno, string materno, int edad)
+        {
+            Curp = curp;
+            Nombre = nombre;
+            Paterno = paterno;
+            Materno = materno;
+            Edad = edad;
+        }
+    }
+}
---
+++ WindowsFormsApplication1/Form2.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using WindowsFormsApplication1.Clases;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form2 : Form
+    {
+        OpenFileDialog ofile = new OpenFileDialog();
+        DialogResult dr;
+
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+
+            dr = ofile.ShowDialog();
+
+            if (dr == DialogResult.OK)
+            {
+                var xml = XDocument.Load(ofile.FileName);
+                var json = ((XElement)xml.FirstNode).Value;
+                var list = JsonConvert.DeserializeObject<List<Fallecido>>(json);
+
+                MessageBox.Show(list.First().Curp + "\n" + list.First().Nombre + "\n" + list.First().Paterno + "\n" + 
+                    list.First().Materno + "\n" + list.First().Edad);
+            }
+        }
+    }
+}
